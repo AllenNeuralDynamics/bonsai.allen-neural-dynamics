ed336ab2659cd5a81abcd3efc44744a5ba52580d
bruno-f-cruz <bfpcruz@gmail.com>
2023-11-02 17:10:38 -0700

    Update package with generic message operators

---
+++ src/AllenNeuralDynamics.Zaber/GenericCommandMultiResponse.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reactive.Linq;
+using Bonsai;
+using Zaber.Motion.Ascii;
+
+namespace AllenNeuralDynamics.Zaber
+{
+    /// <summary>
+    /// Represents an operator that sends a command to a <see cref="ZaberDevice"/> expecting multiple <see cref="Response[]"/>.
+    /// </summary>
+    [Description("Returns the result of a command.")]
+    public class GenericCommandMultiResponse : Combinator<string, Response[]>
+    {
+        /// <summary>
+        /// Gets or sets the COM port or alias of the target <see cref="ZaberDevice"/>
+        /// </summary>
+        [TypeConverter(typeof(PortNameConverter))]
+        [Description("The name of the serial port used to communicate with the manipulator.")]
+        public string PortName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the axis of the manipulator to be controlled.
+        /// </summary>
+        [Description("The axis index to be actuated.")]
+        public int? Axis { get; set; }
+
+        /// <summary>
+        /// Sends a command to the device expecting multiple replies.
+        /// </summary>
+        /// <returns>
+        /// On each event, it will emit the response of the device.
+        /// </returns>
+        public override IObservable<Response[]> Process(IObservable<string> source)
+        {
+            return Observable.Using(
+                async token => await ZaberDeviceManager.ReserveConnectionAsync(PortName),
+                async (connection, cancellationToken) => source.Select(value => 
+                    Observable.FromAsync( async token =>
+                        await connection.Device.GenericCommandMultiResponse(Axis, value)
+                    )).Concat());
+        }
+    }
+}
---
+++ src/AllenNeuralDynamics.Zaber/GenericCommandNoResponse.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using Bonsai;
+
+
+namespace AllenNeuralDynamics.Zaber
+{
+    /// <summary>
+    /// Represents an operator sends a command to a  <see cref="ZaberDevice"/> without expecting a reply.
+    /// </summary>
+    [Description("Sends a command without expecting a result.")]
+    public class GenericCommandNoResponse : Sink<string>
+    {
+        /// <summary>
+        /// Gets or sets the COM port or alias of the target <see cref="ZaberDevice"/>
+        /// </summary>
+        [TypeConverter(typeof(PortNameConverter))]
+        [Description("The name of the serial port used to communicate with the manipulator.")]
+        public string PortName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the axis of the manipulator to be controlled.
+        /// </summary>
+        [Description("The index of the axis of the manipulator to be controlled.")]
+        public int? Axis { get; set; }
+
+        /// <summary>
+        /// Sends a command to the device wihout expecting a reply.
+        /// </summary>
+        /// <returns>
+        /// It will propagate the original sequence.
+        /// </returns>
+        public override IObservable<string> Process(IObservable<string> source)
+        {
+            return Observable.Using(
+                cancellationToken => ZaberDeviceManager.ReserveConnectionAsync(PortName),
+                (connection, cancellationToken) =>
+                {
+                    return Task.FromResult(source.Do(value =>
+                    {
+                        lock (connection.Device)
+                        {
+                            connection.Device.GenericCommandNoResponse(Axis, value);
+                        }
+                    }));
+                });
+        }
+    }
+}
