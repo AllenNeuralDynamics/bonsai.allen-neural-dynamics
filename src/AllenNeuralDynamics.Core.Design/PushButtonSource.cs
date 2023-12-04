dc8ec9d69319942e647c9db145c8c08c07bc22f1
bruno-f-cruz <bfpcruz@gmail.com>
2023-12-04 15:02:30 -0800

    Fix operator

--- src/AllenNeuralDynamics.Core.Design/PushButtonSource.cs
+++ src/AllenNeuralDynamics.Core.Design/PushButtonSource.cs
@@ -3,6 +3,8 @@ using System.ComponentModel;
 
 namespace AllenNeuralDynamics.Core.Design
 {
+    [Combinator]
+    [WorkflowElementCategory(ElementCategory.Source)]
     [TypeVisualizer(typeof(PushButtonVisualizer))]
     [Description("Generates a sequence of manual annotations or alerts about the experiment.")]
     public class PushButtonSource : PushButton
