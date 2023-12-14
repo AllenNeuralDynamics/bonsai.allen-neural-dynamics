2f4ab0fd4baf5ca1fbc14200dd2a2bf7f9d5e1b6
bruno-f-cruz <bfpcruz@gmail.com>
2023-12-14 10:21:19 -0800

    Add operator to convert a double into a TimeSpan object

---
+++ src/AllenNeuralDynamics.Core/TimeSpanFromSeconds.cs
@@ -0,0 +1,20 @@
+﻿using Bonsai;
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace AllenNeuralDynamics.Core
+{
+    [Combinator]
+    [Description("Converts a numerical value in seconds to a TimeSpan object.")]
+    [WorkflowElementCategory(ElementCategory.Transform)]
+
+    public class TimeSpanFromSeconds
+    {
+        public IObservable<TimeSpan> Process(IObservable<double> source)
+        {
+            return source.Select(value => TimeSpan.FromSeconds(value));
+        }
+    }
+}
