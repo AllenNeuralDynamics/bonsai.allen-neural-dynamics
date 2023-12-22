15bca7fa69900559f05ed522821a03e28aeab968
bruno-f-cruz <bfpcruz@gmail.com>
2023-12-21 17:45:19 -0800

    Add converter for Zaber package Units Enum.

---
+++ src/AllenNeuralDynamics.Zaber/UnitsConverter.cs
@@ -0,0 +1,69 @@
+﻿using System.ComponentModel;
+using Zaber.Motion;
+
+namespace AllenNeuralDynamics.Zaber
+{
+
+    public class PositionUnitsConverter: EnumConverter
+    {
+        public PositionUnitsConverter() : base(typeof(Units))
+        {
+        }
+
+        public override StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+        {
+            return new StandardValuesCollection(new[]
+            {
+                Units.Native,
+                Units.Length_Nanometres,
+                Units.Length_Micrometres,
+                Units.Length_Millimetres,
+                Units.Length_Centimetres,
+                Units.Length_Metres,
+                Units.Length_Inches
+            });
+        }
+    }
+
+    public class VelocityUnitsConverter : EnumConverter
+    {
+        public VelocityUnitsConverter() : base(typeof(Units))
+        {
+        }
+
+        public override StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+        {
+            return new StandardValuesCollection(new[]
+            {
+                Units.Native,
+                Units.Velocity_NanometresPerSecond,
+                Units.Velocity_MicrometresPerSecond,
+                Units.Velocity_MillimetresPerSecond,
+                Units.Velocity_CentimetresPerSecond,
+                Units.Velocity_MetresPerSecond,
+                Units.Velocity_InchesPerSecond,
+            });
+        }
+    }
+
+    public class AccelerationUnitsConverter : EnumConverter
+    {
+        public AccelerationUnitsConverter() : base(typeof(Units))
+        {
+        }
+
+        public override StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+        {
+            return new StandardValuesCollection(new[]
+            {
+                Units.Native,
+                Units.Acceleration_NanometresPerSecondSquared,
+                Units.Acceleration_MicrometresPerSecondSquared,
+                Units.Acceleration_MillimetresPerSecondSquared,
+                Units.Acceleration_CentimetresPerSecondSquared,
+                Units.Acceleration_MetresPerSecondSquared,
+                Units.Acceleration_InchesPerSecondSquared,
+            });
+        }
+    }
+}
