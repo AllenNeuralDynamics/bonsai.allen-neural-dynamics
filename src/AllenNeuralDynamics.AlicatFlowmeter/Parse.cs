754d2846bc319f293c302c59e69c2b0a9238c596
bruno-f-cruz <bfpcruz@gmail.com>
2024-01-05 16:34:35 -0800

    Add package to interface with Flowmeter

---
+++ src/AllenNeuralDynamics.AlicatFlowmeter/Parse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reactive.Linq;
+using Bonsai;
+
+
+namespace AllenNeuralDynamics.AlicatFlowmeter
+{
+    [Description("Parses an incoming string into a FlowmeterDataFrame.")]
+    public class Parse : Transform<string, FlowmeterDataframe>
+    {
+        public override IObservable<FlowmeterDataframe> Process(IObservable<string> source)
+        {
+            return source.Select(x => new FlowmeterDataframe(x));
+        }
+    }
+}
