ca94ec1359ea1ed04f02824a60207c128747c512
bruno-f-cruz <bfpcruz@gmail.com>
2024-01-31 06:05:43 -0800

    Fix operator namespace

--- src/AllenNeuralDynamics.Core.Design/MessageBox.cs
+++ src/AllenNeuralDynamics.Core.Design/MessageBox.cs
@@ -6,41 +6,45 @@ using System.Reactive.Linq;
 using System.Windows.Forms;
 using System.Threading.Tasks;
 
-[Combinator]
-[Description("Creates a custom message box dialog window.")]
-[WorkflowElementCategory(ElementCategory.Sink)]
-public class MessageBox
+
+namespace AllenNeuralDynamics.Core.Design
 {
-    private string text = "";
-    public string Text
+    [Combinator]
+    [Description("Creates a custom message box dialog window.")]
+    [WorkflowElementCategory(ElementCategory.Sink)]
+    public class MessageBox
     {
-        get { return text; }
-        set { text = value; }
-    }
-
-    private string title = "Warning";
-    public string Title
-    {
-        get { return title; }
-        set { title = value; }
-    }
-
-
-    private MessageBoxIcon messageBoxIcon = MessageBoxIcon.Warning;
-    public MessageBoxIcon MessageBoxIcon
-    {
-        get { return messageBoxIcon; }
-        set { messageBoxIcon = value; }
-    }
-
-
-    public IObservable<TSource> Process<TSource>(IObservable<TSource> source)
-    {
-        return source.Select(value =>
+        private string text = "";
+        public string Text
         {
-            var task = new Task(() => System.Windows.Forms.MessageBox.Show(text, title, MessageBoxButtons.OK, MessageBoxIcon));
-            task.Start();
-            return value;
-        });
+            get { return text; }
+            set { text = value; }
+        }
+
+        private string title = "Warning";
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+
+        private MessageBoxIcon messageBoxIcon = MessageBoxIcon.Warning;
+        public MessageBoxIcon MessageBoxIcon
+        {
+            get { return messageBoxIcon; }
+            set { messageBoxIcon = value; }
+        }
+
+
+        public IObservable<TSource> Process<TSource>(IObservable<TSource> source)
+        {
+            return source.Select(value =>
+            {
+                var task = new Task(() => System.Windows.Forms.MessageBox.Show(text, title, MessageBoxButtons.OK, MessageBoxIcon));
+                task.Start();
+                return value;
+            });
+        }
     }
 }
