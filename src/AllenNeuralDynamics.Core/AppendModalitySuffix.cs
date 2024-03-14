4dfd91c7db10c4d79970c7178ba56692f61db050
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-03-14 13:10:31 -0700

    Add new modality operator to logging package

---
+++ src/AllenNeuralDynamics.Core/AppendModalitySuffix.cs
@@ -0,0 +1,42 @@
+﻿using Bonsai;
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace AllenNeuralDynamics.Core.Logging
+{
+    [Description("Outputs a known data modality string from a dropdown list.")]
+    public class AppendModalitySuffix : Transform<string, string>
+    {
+        public Modality? Modality { get; set; } = null;
+
+        public override IObservable<string> Process(IObservable<string> source)
+        {
+            return source.Select(value =>
+            {
+                return Path.Combine(value, Modality.HasValue ? Modality.ToString() : "");
+            });
+        }
+    }
+
+    public enum Modality
+    {
+        Other = 0,
+        Behavior = 1,
+        Confocal = 2,
+        Ecephys = 3,
+        BehaviorVideos = 4,
+        Electromyography = 5,
+        Fib = 6,
+        Fmost = 7,
+        Icephys = 8,
+        Isi = 9,
+        Merfish = 10,
+        Mri = 11,
+        POphys = 12,
+        Slap = 13,
+        Spim = 14,
+    }
+}
