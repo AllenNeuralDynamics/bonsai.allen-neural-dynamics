7427a643f8807164c821c17ad1fd992c330de075
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-03-21 13:17:50 -0700

    Align build with actions

--- docs/build.ps1
+++ docs/build.ps1
@@ -1,27 +1,3 @@
-# Build Bonsai packages
-$baseDir = (Get-Item -Path "..\src" -Verbose).FullName
-$folderPaths = Get-ChildItem -Path $baseDir -Directory
-foreach ($folderPath in $folderPaths) {
-    $slnPath = Join-Path -Path $folderPath.FullName -ChildPath ($folderPath.Name + ".sln")
-    if (Test-Path -Path $slnPath) {
-        Write-Host ("Building: " + $slnPath)
-        $command = ("msbuild -t:restore " + $slnPath)
-        Invoke-Expression $command
-        $command = ("msbuild " + $slnPath + " /p:Configuration=Release")
-        Invoke-Expression $command
-    }
-}
-
-# Build Harp Interfaces
-$harp_solutions = Get-ChildItem .\harp_devices_src\harp.device.*\software\bonsai\Interface\*.sln
-foreach ($slnPath in $harp_solutions) {
-    Write-Host ("Building: " + $slnPath)
-    $command = ("msbuild -t:restore " + $slnPath)
-    Invoke-Expression $command
-    $command = ("msbuild " + $slnPath + " /p:Configuration=Release")
-    Invoke-Expression $command
-}
-
 # Build device tables
 $files = Get-ChildItem .\harp_devices_src\harp.device.*\software\bonsai\device.yml
 foreach ($file in $files)
