fe61697316226abe648b94740f2dba4de5fb7583
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-03-21 12:37:16 -0700

    Merge build in a single file

--- docs/build.ps1
+++ docs/build.ps1
@@ -1,8 +1,44 @@
+# Build Bonsai packages
+$baseDir = (Get-Item -Path "..\src" -Verbose).FullName
+$folderPaths = Get-ChildItem -Path $baseDir -Directory
+foreach ($folderPath in $folderPaths) {
+    $slnPath = Join-Path -Path $folderPath.FullName -ChildPath ($folderPath.Name + ".sln")
+    if (Test-Path -Path $slnPath) {
+        Write-Host ("Building: " + $slnPath)
+        $command = ("msbuild -t:restore " + $slnPath)
+        Invoke-Expression $command
+        $command = ("msbuild " + $slnPath + " /p:Configuration=Release")
+        Invoke-Expression $command
+    }
+}
+
+# Build Harp Interfaces
+$harp_solutions = Get-ChildItem .\harp_devices_src\harp.device.*\software\bonsai\Interface\*.sln
+foreach ($slnPath in $harp_solutions) {
+    Write-Host ("Building: " + $slnPath)
+    $command = ("msbuild -t:restore " + $slnPath)
+    Invoke-Expression $command
+    $command = ("msbuild " + $slnPath + " /p:Configuration=Release")
+    Invoke-Expression $command
+}
+
+# Build device tables
+$files = Get-ChildItem .\harp_devices_src\harp.device.*\software\bonsai\device.yml
+foreach ($file in $files)
+{
+    Write-Output "Generating schema tables for $file..."
+    dotnet run --project .\harp_devices_src\harp.schemaprocessor $file .\harp_devices
+}
+
+
+# Find assemblies to build workflows
 $baseDir = (Get-Item -Path "..\src" -Verbose).FullName
 $folderPaths = Get-ChildItem -Path $baseDir -Directory
 $sufix = "bin\Release\net472"
 $packages = @()
 
+
+# find package assemblies
 foreach ($folderPath in $folderPaths) {
     $snlPath = Join-Path -Path $folderPath.FullName -ChildPath ($folderPath.Name + ".sln")
     if (Test-Path -Path $snlPath) {
@@ -11,12 +47,18 @@ foreach ($folderPath in $folderPaths) {
     }
 }
 
-$harp_solutions = Get-ChildItem ..\src\HarpDevices\harp.device.*\software\bonsai\Interface\*.sln
+# find device assemblies
+$sufix = "bin\Release\net472"
+
+$harp_solutions = Get-ChildItem .\harp_devices_src\harp.device.*\software\bonsai\Interface\*.sln
 foreach ($solution in $harp_solutions) {
     $libPath = Join-Path -Path $folderPath.FullName -ChildPath $sufix # grab the lib
     $packages += $solution
 }
 
+
 Write-Host ("Found the following packages: " + $packages)
 .\bonsai\modules\Export-Image.ps1 $packages
+
+# Build documentation
 dotnet docfx @args
--- docs/build_harp_devices.ps1
+++
@@ -1,6 +0,0 @@
-$files = Get-ChildItem .\src\HarpDevices\harp.device.*\software\bonsai\device.yml
-foreach ($file in $files)
-{
-    Write-Output "Generating schema tables for $file..."
-    dotnet run --project .\src\harp.schemaprocessor $file .\docs\harp_devices
-}
