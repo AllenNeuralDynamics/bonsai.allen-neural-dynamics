330025af831de88ffb600af0ac0e622696629894
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-03-21 11:40:32 -0700

    Build harp devices interfaces

--- docs/build.ps1
+++ docs/build.ps1
@@ -2,6 +2,7 @@ $baseDir = (Get-Item -Path "..\src" -Verbose).FullName
 $folderPaths = Get-ChildItem -Path $baseDir -Directory
 $sufix = "bin\Release\net472"
 $packages = @()
+
 foreach ($folderPath in $folderPaths) {
     $snlPath = Join-Path -Path $folderPath.FullName -ChildPath ($folderPath.Name + ".sln")
     if (Test-Path -Path $snlPath) {
@@ -9,6 +10,13 @@ foreach ($folderPath in $folderPaths) {
         $packages += $libPath
     }
 }
+
+$harp_solutions = Get-ChildItem ..\src\HarpDevices\harp.device.*\software\bonsai\Interface\*.sln
+foreach ($solution in $harp_solutions) {
+    $libPath = Join-Path -Path $folderPath.FullName -ChildPath $sufix # grab the lib
+    $packages += $solution
+}
+
 Write-Host ("Found the following packages: " + $packages)
 .\bonsai\modules\Export-Image.ps1 $packages
 dotnet docfx @args
