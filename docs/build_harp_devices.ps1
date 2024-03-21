86822655b218cb267adf03ce05b4b3abd4e91b11
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-03-21 08:02:18 -0700

    Add harp device building script

---
+++ docs/build_harp_devices.ps1
@@ -0,0 +1,6 @@
+$files = Get-ChildItem .\src\HarpDevices\harp.device.*\software\bonsai\device.yml
+foreach ($file in $files)
+{
+    Write-Output "Generating schema tables for $file..."
+    dotnet run --project .\src\harp.schemaprocessor $file .\apispec
+}
