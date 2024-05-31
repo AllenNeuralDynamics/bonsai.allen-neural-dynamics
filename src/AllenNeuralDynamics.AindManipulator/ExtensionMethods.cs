d76addc59f0bce9cb18e1f50d029fdb6d612201c
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-05-31 09:12:21 -0700

    Add arithmetic overloads to ManipulatorPosition

---
+++ src/AllenNeuralDynamics.AindManipulator/ExtensionMethods.cs
@@ -0,0 +1,48 @@
+﻿namespace AllenNeuralDynamics.AindManipulator {
+    public partial class ManipulatorPosition
+    {
+        public static ManipulatorPosition operator +(ManipulatorPosition el1, ManipulatorPosition el2)
+        {
+            return new ManipulatorPosition()
+            {
+                X = el1.X + el2.X,
+                Y1 = el1.Y1 + el2.Y1,
+                Y2 = el1.Y2 + el2.Y2,
+                Z = el1.Z + el2.Z
+            };
+        }
+
+        public static ManipulatorPosition operator -(ManipulatorPosition el1, ManipulatorPosition el2)
+        {
+            return new ManipulatorPosition()
+            {
+                X = el1.X - el2.X,
+                Y1 = el1.Y1 - el2.Y1,
+                Y2 = el1.Y2 - el2.Y2,
+                Z = el1.Z - el2.Z
+            };
+        }
+
+        public static ManipulatorPosition operator *(ManipulatorPosition el1, float gain)
+        {
+            return new ManipulatorPosition()
+            {
+                X = el1.X * gain,
+                Y1 = el1.Y1 * gain,
+                Y2 = el1.Y2 * gain,
+                Z = el1.Z * gain
+            };
+        }
+        public static ManipulatorPosition operator *(ManipulatorPosition el1, ManipulatorPosition el2)
+        {
+            return new ManipulatorPosition()
+            {
+                X = el1.X * el2.X,
+                Y1 = el1.Y1 * el2.Y1,
+                Y2 = el1.Y2 * el2.Y2,
+                Z = el1.Z * el2.Z
+            };
+
+        }
+    }
+}
