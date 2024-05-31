06d87734c171c291a1f1319c07fcc74b371c968d
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-05-31 13:33:57 -0700

    Add operator to check the existance of a dictionary key

---
+++ src/AllenNeuralDynamics.AindManipulator/ContainsKey.cs
@@ -0,0 +1,25 @@
+﻿using Bonsai;
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace AllenNeuralDynamics.Core
+{
+    [Combinator]
+    [Description("Returns true if the dictionary contains the specified key.")]
+    [WorkflowElementCategory(ElementCategory.Transform)]
+    public class ContainsKey
+    {
+        public IObservable<bool> Process<TKey, TValue>(IObservable<Tuple<TKey, IDictionary<TKey, TValue>>> source)
+        {
+            return source.Select(value => { return value.Item2.ContainsKey(value.Item1); });
+        }
+
+        public IObservable<bool> Process<TKey, TValue>(IObservable<Tuple<IDictionary<TKey, TValue>, TKey>> source)
+        {
+            return source.Select(value => { return value.Item1.ContainsKey(value.Item2); });
+        }
+    }
+}
