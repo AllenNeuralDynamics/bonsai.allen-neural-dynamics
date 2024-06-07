34d1d068cfb71ce4c36e76f3190cd4e9a37f2b68
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-06-07 11:27:14 -0700

    Exclude IncludeWorkflows from toolbox

---
+++ src/AllenNeuralDynamics.AindManipulator/IncludeWorkflowMetadata.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace AllenNeuralDynamics.AindManipulator
+{
+    [DesignTimeVisible(false)]
+    public static class ConfigureMotor
+    {
+    }
+
+    [DesignTimeVisible(false)]
+    public static class HomeAxis
+    {
+    }
+
+    [DesignTimeVisible(false)]
+    public static class ModifyMaskMotorRegister
+    {
+    }
+
+    [DesignTimeVisible(false)]
+    public static class OffsetMotorRegister
+    {
+    }
+}
