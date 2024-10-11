43729705a1a9be4408cd43456acd23ffd40f976a
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-10-11 14:09:12 -0700

    Build against version 0.8.0 of AindBehaviorServices

--- src/AllenNeuralDynamics.AindBehaviorServices/AindBehaviorServicesDataTypes.cs
+++ src/AllenNeuralDynamics.AindBehaviorServices/AindBehaviorServicesDataTypes.cs
@@ -387,7 +387,7 @@ namespace AllenNeuralDynamics.AindBehaviorServices.DataTypes
     public abstract partial class DataTypes
     {
     
-        private string _aindBehaviorServicesPkgVersion = "0.8.0-rc1";
+        private string _aindBehaviorServicesPkgVersion = "0.8.0";
     
         private string _version = "0.1.1";
     
