a598acf7d8df4b19f2432663f63d8267fec863e9
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-10-20 10:31:48 -0700

    Add visualizer operator to demux spinnaker camera stream

---
+++ src/AllenNeuralDynamics.Core.Design/AccumulateToList.cs
@@ -0,0 +1,25 @@
+﻿using Bonsai;
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Collections.Generic;
+
+namespace AllenNeuralDynamics.Core.Design
+{
+    [Combinator]
+    [Description("Accumulates an incoming sequence of elements and outputs a list each time a new element arrives")]
+    [WorkflowElementCategory(ElementCategory.Transform)]
+    public class AccumulateToList
+    {
+        public IObservable<IList<T>> Process<T>(IObservable<T> source)
+        {
+            var list = new List<T>();
+            return source.Select(value =>
+            {
+                list.Add(value);
+                return list;
+            });
+        }
+    }
+}
